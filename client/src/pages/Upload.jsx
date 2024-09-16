602e6d5d6223e5c35cda182715bc4411830d61aa
arka26095656 <arkaprava.kgp@gmail.com>
2024-09-16 19:30:37 +0530

    First Commit

---
+++ client/src/pages/Upload.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UploadNote from "../components/UploadNote";
+
+const Upload = () => {
+  return (
+    <div className="flex h-heightWithoutNavbar items-center justify-center">
+      <UploadNote />
+    </div>
+  );
+};
+
+export default Upload;
